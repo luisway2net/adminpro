6891a46a61ffc8cdf09d7538208dca82c3f5ee31
Way2net <way2net@IMAC-Luis.local>
2020-09-28 13:12:37 -0300

    antes de actualizar cli y core angular

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+import { SharedModule } from '../shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component
+  ],
+  imports: [
+    CommonModule,
+    SharedModule
+  ]
+})
+export class PagesModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+
+
+@NgModule({
+  declarations: [
+    NopagefoundComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
+  ],
+  exports: [
+    NopagefoundComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
+  ],
+
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
